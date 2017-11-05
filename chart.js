26edadc833f28e8bcc79bcd39b307809268746c9
Τριανταφυλλίδης Βασίλης <32864583+p13tria@users.noreply.github.com>
2017-11-05 18:46:36 +0200

    Changing color order

--- chart.js
+++ chart.js
@@ -19,9 +19,10 @@ var entityCentres = {
 		society: {x: w / 1.12, y: h  / 3.2 },
 		pub: {x: w / 1.8, y: h / 2.8},
 		individual: {x: w / 3.65, y: h / 3.3},
-	};
+	}; 
 
-var fill = d3.scale.ordinal().range(["#04771D", "#cfcd29", "#501208"]);
+//var fill = d3.scale.ordinal().range(["#04771D", "#cfcd29", "#501208"]);
+var fill = d3.scale.ordinal().range(["#cfcd29", "#04771D", "#501208"]);
 
 var svgCentre = { 
     x: w / 3.6, y: h / 2
