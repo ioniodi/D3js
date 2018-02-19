ec1edbd6b263eecd522b3d7375d91c0158fe5138
p15kivr <p15kivr@ionio.gr>
2018-02-19 20:41:34 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -41,11 +41,8 @@ var tooltip = d3.select("#chart")
 
 var comma = d3.format(",.0f");
 
-var button = new Audio("FB_Messenger_sound.mp3");    
-
 function transition(name) {
 	if (name === "all-donations") {
-		button.play();
 		$("#initial-content").fadeIn(250);
 		$("#value-scale").fadeIn(1000);
 		$("#view-donor-type").fadeOut(250);
@@ -55,7 +52,6 @@ function transition(name) {
 		//location.reload();
 	}
 	if (name === "group-by-party") {
-		button.play();
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
@@ -64,7 +60,6 @@ function transition(name) {
 		return partyGroup();
 	}
 	if (name === "group-by-donor-type") {
-		button.play();
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
@@ -72,8 +67,7 @@ function transition(name) {
 		$("#view-donor-type").fadeIn(1000);
 		return donorType();
 	}
-	if (name === "group-by-money-source"){
-		button.play();
+	if (name === "group-by-money-source")
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
