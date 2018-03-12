bd26b60bda09f545bea71dedb727ceacb9e41fda
p15sidi <p15sidi@ionio.gr>
2018-03-13 01:02:56 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -21,7 +21,7 @@ var entityCentres = {
 		individual: {x: w / 3.65, y: h / 3.3},
 	};
 
-var fill = d3.scale.ordinal().range(['#ABD91F', '#FF1FAB', '#991337']);
+var fill = d3.scale.ordinal().range(['#616161', '#4E342E', '#DD2C00']);
 
 var svgCentre = { 
     x: w / 3.6, y: h / 2
