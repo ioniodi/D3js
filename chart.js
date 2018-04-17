c7aaed8b1bd70a46af0725801a511f6856af8c6b
Spiros Vergis <p15verg@ionio.gr>
2018-04-17 22:07:35 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -403,7 +403,7 @@ function mouseover(d, i) {
     .style("top", (parseInt(d3.select(this).attr("cy") - (d.radius+150)) + offset.top) + "px")
 		.html(infoBox)
 			.style("display","block");
-	responsiveVoice.speak("The donor        " + d.donor + "       has donated an amount of     " + d.amount + "          british pounds!");
+	responsiveVoice.speak("The donor        " + d.donor + "       has donated an amount of     " + d.amount + "       british pounds");
 	
 	
 	}
