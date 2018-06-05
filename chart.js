321029c1391bcfa720e76a76891aa367717f0f01
elly1323 <ellypapagrigoriou@gmail.com>
2018-06-05 14:47:19 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -21,7 +21,7 @@ var entityCentres = {
 		individual: {x: w / 3.65, y: h / 3.3},
 	};
 
-var fill = d3.scale.ordinal().range(["#b3abbc", "#2ae05e", "#c900e8"]);
+var fill = d3.scale.ordinal().range(["#F02233", "#087FBD", "#FDBB30"]);
 
 var svgCentre = { 
     x: w / 3.6, y: h / 2
